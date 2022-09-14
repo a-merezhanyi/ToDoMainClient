b399593c23a1462b8908a1d5345cad1ec6b3ec8c
Tolik Merezhanyi <a.merezhanyi@gmail.com>
2022-09-14 20:33:37 +0200

    add fetch

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+import { checkHeath } from "../util/http";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  const [status, setStatus] = useState("unknown");
+  const checkStatus = async () => {
+    const res = await checkHeath();
+    setStatus(res.message || res);
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Create Task</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Button title="check" onPress={checkStatus} />
+      <Text style={styles.title}>Status: {status}</Text>
     </View>
   );
 }
@@ -17,16 +32,16 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
